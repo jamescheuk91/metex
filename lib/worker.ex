87770c6138fb9492f63951ab5f5e621106feaffb
James Cheuk <jccf091@gmail.com>
2017-06-25 18:00:05 +0800

    impl GenServer behaviors in Metex.Worker

--- lib/worker.ex
+++ lib/worker.ex
@@ -1,29 +1,64 @@
 defmodule Metex.Worker do
+  use GenServer
   require Logger
 
-  def loop do
-    receive do
-      {sender_pid, location} ->
-        send(sender_pid, {:ok, temperature_of(location)})
-      _ ->
-        Logger.info "don't know how to process this message"
+  ## Client API
 
-    after
-      1_000 -> loop()
-    end
+  def start_link(opts \\ []) do
+    GenServer.start_link(__MODULE__, :ok, opts)
   end
 
-  def temperature_of(location) do
-    result = url_for(location) |> HTTPoison.get |> parse_repsonse
+  def init(:ok) do
+    {:ok, %{}}
+  end
 
-    case result do
+  def init(state) do
+    {:ok, state}
+  end
+
+  def get_state(pid) do
+    GenServer.call(pid, :get_state)
+  end
+
+  def get_temperature(pid, location) do
+    GenServer.call(pid, {:location, location})
+  end
+
+  ## Server API
+  ## GenServer Callbacks
+
+  def handle_call(:get_state, _from, state) do
+    {:reply, state, state}
+  end
+
+  def handle_call({:location, location}, _form, state) do
+    case temperature_of(location) do
       {:ok, temp} ->
-        "#{location}: #{temp}°C"
-      :error ->
-        "#{location} not found"
+        new_state = update_state(state, location)
+        {:reply, "#{temp}°C", new_state}
+      _ ->
+        {:reply, :error, state}
     end
   end
 
+
+  ## Helper Functions
+
+  # def loop do
+  #   receive do
+  #     {sender_pid, location} ->
+  #       send(sender_pid, {:ok, temperature_of(location)})
+  #     _ ->
+  #       Logger.info "don't know how to process this message"
+  #
+  #   after
+  #     1_000 -> loop()
+  #   end
+  # end
+
+  defp temperature_of(location) do
+    url_for(location) |> HTTPoison.get |> parse_repsonse
+  end
 
   defp url_for(location) do
     location = URI.encode(location)
@@ -39,6 +74,11 @@ defmodule Metex.Worker do
 
   defp parse_repsonse(_) do
     :error
+  end
+
+  defp update_state(old_state, location) do
+    initial = 1
+    Map.update(old_state, location, initial, &(&1 + 1))
   end
 
   defp compute_temperature(json) do
